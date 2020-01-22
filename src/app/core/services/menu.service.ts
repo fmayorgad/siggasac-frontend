778d5b14d3d914408519c11000dd837ba7087057
Fabio Alberto Mayorga Duarte <fabiomayorgad@hotmail.com>
2020-01-22 14:17:17 -0500

    hf

--- src/app/core/services/menu.service.ts
+++ src/app/core/services/menu.service.ts
@@ -148,109 +148,118 @@ export class MenuService {
   getAll(): Menu[] {
     console.log("creando menu desde login")
     let localvariable: any = JSON.parse(localStorage.getItem('currentUser'));
-    const localuser = JSON.parse(localStorage.getItem('currentUser'));
-    const helper = new JwtHelperService();
-    localvariable = localvariable.token;
-    let decodedToken = helper.decodeToken(localvariable);
-    const menucopy = [];
-    const t12 = this.menu;
 
-    if (!decodedToken) {
-      //this.router.navigate(['/']);
-    }
+    if (localStorage.getItem('currentUser')) {
 
-    // Filtering menu
-    // se hace asi por ahora porque no esta el valor en el endpoint
 
-    // se recorre el menu y se elimina lo que no se encuentre en el token
-    let i1 = 0;
-    let incomingmenu = decodedToken.menus;
+      const localuser = JSON.parse(localStorage.getItem('currentUser'));
+      const helper = new JwtHelperService();
+      localvariable = localvariable.token;
+      let decodedToken = helper.decodeToken(localvariable);
+      const menucopy = [];
+      const t12 = this.menu;
 
-    // Se realiza el proceso con Objetos y no con arrays para reutilizar dichos onbjetos en la validación de permisos como crear, ver eliminar etc,
-    // y que no sea necesario filtrar un array (usando una funcion especifica para dicho proceso) y validar si mostrar o no la opción
-
-    // converción del menu del token
-    const tokenmenu = {};
-    for (const i of incomingmenu) {
-      tokenmenu[i.name] = {};
-      tokenmenu[i.name].name = i.name;
-      if (i.submenus.length === 0) {
-        tokenmenu[i.name].permissions = i.permissions;
+      if (!decodedToken) {
+        //this.router.navigate(['/']);
       }
-      if (i.submenus.length > 0) {
-        for (const i2 of i.submenus) {
-          if (!tokenmenu[i.name].children) {
-            tokenmenu[i.name].children = {};
-            tokenmenu[i.name].children[i2.name] = {};
-            tokenmenu[i.name].children[i2.name].permissions = i2.permissions;
-          } else {
-            tokenmenu[i.name].children[i2.name] = {};
-            tokenmenu[i.name].children[i2.name].permissions = i2.permissions;
+
+      // Filtering menu
+      // se hace asi por ahora porque no esta el valor en el endpoint
+
+      // se recorre el menu y se elimina lo que no se encuentre en el token
+      let i1 = 0;
+      let incomingmenu = decodedToken.menus;
+
+      // Se realiza el proceso con Objetos y no con arrays para reutilizar dichos onbjetos en la validación de permisos como crear, ver eliminar etc,
+      // y que no sea necesario filtrar un array (usando una funcion especifica para dicho proceso) y validar si mostrar o no la opción
+
+      // converción del menu del token
+      const tokenmenu = {};
+      for (const i of incomingmenu) {
+        tokenmenu[i.name] = {};
+        tokenmenu[i.name].name = i.name;
+        if (i.submenus.length === 0) {
+          tokenmenu[i.name].permissions = i.permissions;
+        }
+        if (i.submenus.length > 0) {
+          for (const i2 of i.submenus) {
+            if (!tokenmenu[i.name].children) {
+              tokenmenu[i.name].children = {};
+              tokenmenu[i.name].children[i2.name] = {};
+              tokenmenu[i.name].children[i2.name].permissions = i2.permissions;
+            } else {
+              tokenmenu[i.name].children[i2.name] = {};
+              tokenmenu[i.name].children[i2.name].permissions = i2.permissions;
+            }
           }
         }
       }
-    }
 
-    // conversion del menu de la app
-    const appmenu = {}
-    for (const i of this.menu) {
-      appmenu[i.state] = {};
-      appmenu[i.state].name = i.name;
-      appmenu[i.state].state = i.state;
-      appmenu[i.state].type = i.type;
-      appmenu[i.state].icon = i.icon;
+      // conversion del menu de la app
+      const appmenu = {}
+      for (const i of this.menu) {
+        appmenu[i.state] = {};
+        appmenu[i.state].name = i.name;
+        appmenu[i.state].state = i.state;
+        appmenu[i.state].type = i.type;
+        appmenu[i.state].icon = i.icon;
 
-      if (i.children) {
-        for (let i2 of i.children) {
-          if (!appmenu[i.state].children) {
-            appmenu[i.state].children = {};
-            appmenu[i.state].children[i2.state] = {};
-            appmenu[i.state].children[i2.state].state = i2.state;
-            appmenu[i.state].children[i2.state].name = i2.name;
-            appmenu[i.state].children[i2.state].type = i2.type;
-          } else {
-            appmenu[i.state].children[i2.state] = {};
-            appmenu[i.state].children[i2.state].state = i2.state;
-            appmenu[i.state].children[i2.state].name = i2.name;
-            appmenu[i.state].children[i2.state].type = i2.type;
+        if (i.children) {
+          for (let i2 of i.children) {
+            if (!appmenu[i.state].children) {
+              appmenu[i.state].children = {};
+              appmenu[i.state].children[i2.state] = {};
+              appmenu[i.state].children[i2.state].state = i2.state;
+              appmenu[i.state].children[i2.state].name = i2.name;
+              appmenu[i.state].children[i2.state].type = i2.type;
+            } else {
+              appmenu[i.state].children[i2.state] = {};
+              appmenu[i.state].children[i2.state].state = i2.state;
+              appmenu[i.state].children[i2.state].name = i2.name;
+              appmenu[i.state].children[i2.state].type = i2.type;
+            }
           }
         }
       }
-    }
-    // se recorre el array de la app y se compara con el array del token, para crear el menu a pintar
-    // tslint:disable-next-line: forin
+      // se recorre el array de la app y se compara con el array del token, para crear el menu a pintar
+      // tslint:disable-next-line: forin
 
-    const finalmenu = [];
+      const finalmenu = [];
 
-    // tslint:disable-next-line: forin
-    for (const i in appmenu) {
-      if (tokenmenu[i]) {
-        let tmp = {
-          icon: appmenu[i].icon,
-          state: appmenu[i].state,
-          type: appmenu[i].type,
-          name: appmenu[i].name
-        };
+      // tslint:disable-next-line: forin
+      for (const i in appmenu) {
+        if (tokenmenu[i]) {
+          let tmp = {
+            icon: appmenu[i].icon,
+            state: appmenu[i].state,
+            type: appmenu[i].type,
+            name: appmenu[i].name
+          };
 
-        // se filtran ahora los submenus
-        if (appmenu[i].children) {
-          let tmp2 = []
-          // se recorren todos los children del token y se pasan al menu final desde el menu app 
-          // (si existe en el token, debe exizstir en el menu app, pues este ultimo tiene a todos)
-          // tslint:disable-next-line: forin
-          for (const i2 in tokenmenu[i].children) {
-            tmp2.push(appmenu[i].children[i2])
+          // se filtran ahora los submenus
+          if (appmenu[i].children) {
+            let tmp2 = []
+            // se recorren todos los children del token y se pasan al menu final desde el menu app 
+            // (si existe en el token, debe exizstir en el menu app, pues este ultimo tiene a todos)
+            // tslint:disable-next-line: forin
+            for (const i2 in tokenmenu[i].children) {
+              tmp2.push(appmenu[i].children[i2])
+            }
+            tmp['children'] = tmp2;
           }
-          tmp['children'] = tmp2;
+
+          finalmenu.push(tmp);
         }
-
-        finalmenu.push(tmp);
       }
-    }
 
-    this.globals.tree = finalmenu;
-    this.globals.nav = tokenmenu;
-    return finalmenu;
+      this.globals.tree = finalmenu;
+      this.globals.nav = tokenmenu;
+      return finalmenu;
+
+    }
+    else{
+      return [];
+    }
   }
 
   set(menu: Menu[]): Menu[] {
@@ -302,7 +311,7 @@ export class MenuService {
     return tmpArr;
   }
 
-  createAppNavigation(){
+  createAppNavigation() {
     console.log(this.menu);
     return "fabio";
   }
